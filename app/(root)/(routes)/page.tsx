a6a98798ed6ccd91adef77ad5a394e03cc050603
LazY <darshitmistry2@gmail.com>
2023-08-24 09:48:02 +0530

    Authentication was done

---
+++ app/(root)/(routes)/page.tsx
@@ -0,0 +1,12 @@
+import { UserButton } from "@clerk/nextjs";
+import React from "react";
+
+const RootPage = () => {
+  return (
+  <div>
+    <UserButton afterSignOutUrl="/"/>
+  </div>
+  )
+};
+
+export default RootPage;
--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-const Home = () => {
-  return (
-    <div className="">Home</div>
-  )
-}
-
-export default Home
