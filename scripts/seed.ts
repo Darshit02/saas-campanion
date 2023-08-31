e0702bd9da0ef5be507df440ae87867f5df12a69
LazY <darshitmistry2@gmail.com>
2023-08-31 09:30:12 +0530

    Prisma setup done and db query was inited -- also Fixing A Bug

---
+++ scripts/seed.ts
@@ -0,0 +1,50 @@
+// const { PrismaClient } = require("@prisma/client");
+// const db = new PrismaClient();
+
+// async function main() {
+//   try {
+//     await db.catagory.createMany({
+//       data: [
+//         { name: "Famous People"},
+//         { name: "Movies & TV"},
+//         { name: "Musicians" },
+//         { name: "Games"},
+//         { name: "Animals" },
+//         { name: "Philosophy"},
+//         {name: "Scientists"},
+//       ],
+//     });
+//   } catch (error) {
+//     console.log("Error Seeding default catagories", error);
+//   } finally {
+//     await db.$disconnect();
+//   }
+// }
+
+// main();
+
+const { PrismaClient } = require('@prisma/client');
+
+const db = new PrismaClient();
+
+async function main() {
+  try {
+    await db.category.createMany({
+      data: [
+        { name: 'Famous People' },
+        { name: 'Movies & TV' },
+        { name: 'Musicians' },
+        { name: 'Games' },
+        { name: 'Animals' },
+        { name: 'Philosophy' },
+        { name: 'Scientists' },
+      ],
+    });
+  } catch (error) {
+    console.error('Error seeding default categories:', error);
+  } finally {
+    await db.$disconnect();
+  }
+}
+
+main();
