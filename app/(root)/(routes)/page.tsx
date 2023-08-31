9064d5a146d1af2870974d637e1fc0fff889f055
LazY <darshitmistry2@gmail.com>
2023-08-31 10:17:25 +0530

    Campanion query was genrated in Prisma

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,10 +1,14 @@
 import SearchInput from "@/components/SearchInput";
+import { Categories } from "@/components/categories";
+import prismadb from "@/lib/prismadb";
 
 
-const RootPage = () => {
+const RootPage = async () => {
+  const categories = await prismadb.category.findMany()
   return (
   <div className="h-full p-4 space-y-2">
     <SearchInput/>
+  <Categories data={categories}/>
   </div>
   )
 };
