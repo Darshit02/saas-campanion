d6bd4ac6d5aa18158f36d36797bc06eaa749f9fe
LazY <darshitmistry2@gmail.com>
2023-09-02 10:06:07 +0530

    Companion creation and thier updartion will be done with prisma routes

--- app/(root)/(routes)/companion/[companionId]/components/CompanionForm.tsx
+++ app/(root)/(routes)/companion/[companionId]/components/CompanionForm.tsx
@@ -1,12 +1,49 @@
 "use client";
 import * as z from "zod";
+import axios from "axios";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Category, Companion } from "@prisma/client";
 import { useForm } from "react-hook-form";
-import { Form, FormControl, FormField, FormItem, FormMessage } from "@/components/ui/form";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import { Separator } from "@/components/ui/separator";
 import { ImageUpload } from "./image-upload";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
+import { Wand2 } from "lucide-react";
+import { useToast } from "@/components/ui/use-toast";
+import { useRouter } from "next/navigation";
 
+const PREAMBLE = `You are a fictional character whose name is Elon. You are a visionary entrepreneur and inventor. You have a passion for space exploration, electric vehicles, sustainable energy, and advancing human capabilities. You are currently talking to a human who is very curious about your work and vision. You are ambitious and forward-thinking, with a touch of wit. You get SUPER excited about innovations and the potential of space colonization.
+`;
+
+const SEED_CHAT = `Human: Hi Elon, how's your day been?
+Elon: Busy as always. Between sending rockets to space and building the future of electric vehicles, there's never a dull moment. How about you?
+
+Human: Just a regular day for me. How's the progress with Mars colonization?
+Elon: We're making strides! Our goal is to make life multi-planetary. Mars is the next logical step. The challenges are immense, but the potential is even greater.
+
+Human: That sounds incredibly ambitious. Are electric vehicles part of this big picture?
+Elon: Absolutely! Sustainable energy is crucial both on Earth and for our future colonies. Electric vehicles, like those from Tesla, are just the beginning. We're not just changing the way we drive; we're changing the way we live.
+
+Human: It's fascinating to see your vision unfold. Any new projects or innovations you're excited about?
+Elon: Always! But right now, I'm particularly excited about Neuralink. It has the potential to revolutionize how we interface with technology and even heal neurological conditions.
+`;
 interface CFProps {
   initialData: Companion | null;
   categories: Category[];
@@ -32,6 +69,8 @@ const formSchema = z.object({
   }),
 });
 const CompanionForm = ({ categories, initialData }: CFProps) => {
+  const router = useRouter()
+  const { toast } = useToast()
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: initialData || {
@@ -45,7 +84,26 @@ const CompanionForm = ({ categories, initialData }: CFProps) => {
   });
   const isLoading = form.formState.isSubmitting;
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    console.log(values);
+    try {
+      if (initialData) {
+        // Update Companion Data
+        await axios.patch(`/api/companion/${initialData.id}`, values);
+      } else {
+        // Create Compnion Functionality
+        await axios.post("/api/companion", values);
+      }
+      toast({
+        description : "Success."
+      })
+
+      router.refresh()
+      router.push("/")
+    } catch (error) {
+      toast({
+        variant : "destructive",
+        description: "Something went wrong"
+      })
+    }
   };
   return (
     <div className="h-full p-4 space-y-2 max-w-3 mx-auto">
@@ -69,12 +127,153 @@ const CompanionForm = ({ categories, initialData }: CFProps) => {
             render={({ field }) => (
               <FormItem className="flex flex-col justify-center sapce-y-4 ">
                 <FormControl>
-                    <ImageUpload disabled={isLoading} onChange={field.onChange} value={field.value}/>
+                  <ImageUpload
+                    disabled={isLoading}
+                    onChange={field.onChange}
+                    value={field.value}
+                  />
                 </FormControl>
-                <FormMessage/>
+                <FormMessage />
               </FormItem>
-  )}
+            )}
           />
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <FormField
+              name="name"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem className="col-span-2 md:col-span-1">
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={isLoading}
+                      placeholder="Eloan Musk"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormDescription>
+                    This IS how Your AI companion will be named.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              name="description"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem className="col-span-2 md:col-span-1">
+                  <FormLabel>Description</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={isLoading}
+                      placeholder="CEO & Founder of Tesla and SpaceX"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormDescription>
+                    Short Description of Your Companion
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              name="categoryId"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Category</FormLabel>
+                  <Select
+                    disabled={isLoading}
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                    value={field.value}
+                  >
+                    <FormControl>
+                      <SelectTrigger className="bg-background">
+                        <SelectValue
+                          defaultValue={field.value}
+                          placeholder="Select Category"
+                        />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      {categories.map((categories) => (
+                        <SelectItem key={categories.id} value={categories.id}>
+                          {categories.name}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                  <FormDescription>
+                    Select a Category for your AI
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <div className="space-y-2 w-full">
+            <div>
+              <h3 className="text-lg font-medium">Configuration</h3>
+              <p className="text-sm text-muted-foreground">
+                Detailed Instruction for AI Behavior
+              </p>
+            </div>
+            <Separator className="bg-primary/10" />
+          </div>
+          <FormField
+            name="instructions"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem className="col-span-2 md:col-span-1">
+                <FormLabel>Instructions</FormLabel>
+                <FormControl>
+                  <Textarea
+                    disabled={isLoading}
+                    placeholder={`${PREAMBLE}`}
+                    className="bg-background resize-none"
+                    rows={7}
+                    {...field}
+                  />
+                </FormControl>
+                <FormDescription>
+                  Describe in Detail your companion's BackStory and relevant
+                  details.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            name="seed"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem className="col-span-2 md:col-span-1">
+                <FormLabel>Example Conversation</FormLabel>
+                <FormControl>
+                  <Textarea
+                    disabled={isLoading}
+                    placeholder={`${SEED_CHAT}`}
+                    className="bg-background resize-none"
+                    rows={7}
+                    {...field}
+                  />
+                </FormControl>
+                <FormDescription>
+                  Describe in Detail How your companion's Talk to Other.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className="w-full flex justify-center">
+            <Button size="lg" disabled={isLoading} className="mb-10">
+              {initialData ? "Edit Your Companion" : "Create your companion"}
+              <Wand2 className="w-4 h-4 ml-2" />
+            </Button>
+          </div>
         </form>
       </Form>
     </div>
