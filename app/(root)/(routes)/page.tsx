376c65e004a3380e1b3487aecd63107f9f2e16c5
LazY <darshitmistry2@gmail.com>
2023-09-03 08:28:21 +0530

    Campanion card was done

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,16 +1,42 @@
+import Companions from "@/components/Companions";
 import SearchInput from "@/components/SearchInput";
 import { Categories } from "@/components/categories";
 import prismadb from "@/lib/prismadb";
 
+interface RPProps {
+  searchParams: {
+    categoryId: string;
+    name: string;
+  };
+}
 
-const RootPage = async () => {
-  const categories = await prismadb.category.findMany()
+const RootPage = async ({ searchParams }: RPProps) => {
+  const data = await prismadb.companion.findMany({
+    where: {
+      categoryId: searchParams.categoryId,
+      name: {
+        search: searchParams.name,
+      },
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+    include: {
+      _count: {
+        select: {
+          messages: true,
+        },
+      },
+    },
+  });
+  const categories = await prismadb.category.findMany();
   return (
-  <div className="h-full p-4 space-y-2">
-    <SearchInput/>
-  <Categories data={categories}/>
-  </div>
-  )
+    <div className="h-full p-4 space-y-2">
+      <SearchInput />
+      <Categories data={categories} />
+      <Companions data={data} />
+    </div>
+  );
 };
 
 export default RootPage;
